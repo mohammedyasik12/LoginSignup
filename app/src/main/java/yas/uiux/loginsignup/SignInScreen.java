ea802d1d003feefa1f839ad4791e542547856415
mohammedyasik12 <mohammedyasik1992@gmail.com>
2023-02-28 16:14:24 +0530

    Sigin/Signup

---
+++ app/src/main/java/yas/uiux/loginsignup/SignInScreen.java
@@ -0,0 +1,89 @@
+package yas.uiux.loginsignup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SignInScreen extends AppCompatActivity {
+    TextView signup_sift;
+    ImageView password_show,password_unshow;
+    EditText edit_text_password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //REMOVE STATUSBAR
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        //REMOVE TOOLBAR
+        try
+        {
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e){}
+
+
+        setContentView(R.layout.activity_sign_in_screen);
+
+        //MOVE TO SIGNUP SCREEN
+
+        signup_sift =(TextView) findViewById(R.id.signup_sift);
+        signup_sift.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getApplicationContext(),SignUpScreen.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+        edit_text_password=(EditText) findViewById(R.id.edit_text_password);
+
+
+        //PASSWORD HIDE BUTTON
+
+        password_show =(ImageView) findViewById(R.id.password_show);
+        password_unshow =(ImageView) findViewById(R.id.password_unshow);
+        password_show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                edit_text_password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                password_unshow.setVisibility(View.VISIBLE);
+                password_show.setVisibility(View.GONE);
+            }
+        });
+
+        //PASSWORD SHOW BUTTON
+
+        password_unshow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                edit_text_password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+
+                password_unshow.setVisibility(View.GONE);
+                password_show.setVisibility(View.VISIBLE);
+
+            }
+        });
+
+
+
+
+    }
+}
